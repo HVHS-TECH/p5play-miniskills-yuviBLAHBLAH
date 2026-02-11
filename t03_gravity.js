c2a6f6bf80c324515f47cfc4a3a135c9e877b17b
Your Name <23096yb@hvhs.school.nz>
2026-02-11 15:14:22 +1300

    work.

--- t03_gravity.js
+++ t03_gravity.js
@@ -3,7 +3,7 @@
 // Sprite falls due to gravity
 // Written by ???
 /*******************************************************/
-	
+
 /*******************************************************/
 // setup()
 /*******************************************************/
@@ -14,19 +14,29 @@ function setup() {
 	rectangle = new Sprite(400, 200, 67, 67, 'd');
 	rectangle.color = 'red';
 	rectangle.rotationSpeed = -5;
+	rectangle.bouncieness = 2;
 
 	blecktangle = new Sprite(700, 200, 100, 100, 'd');
 	blecktangle.color = 'blue';
 	blecktangle.rotationSpeed = 5;
 	blecktangle.vel.x = -5;
+	blecktangle.bouncieness = 2;
+	wallLH = new Sprite(0, height / 2, 8, height, 'k');
+	wallLH.color = 'black';
+
+	wallRH = new Sprite(width, height / 2, 8, height, 'k');
+
+	wallTop = new Sprite(width / 2, 0, width, 8, 'k');
+
+	wallBot = new Sprite(width / 2, height, width, 8, 'k');
 
 }
-	
+
 /*******************************************************/
 // draw()
 /*******************************************************/
 function draw() {
-		background("white");
+	background("white");
 }
 
 /*******************************************************/
--- t06_createAliens.js
+++ t06_createAliens.js
@@ -3,20 +3,59 @@
 // Create aliens on mouse click
 // Written by ???
 /*******************************************************/
-	
+
 /*******************************************************/
 // setup()
 /*******************************************************/
 function setup() {
 	console.log("setup: ");
+	cnv = new Canvas(1000, 1000);
+	world.gravity.y = 10;
+	blecktangle = new Sprite(700, 200, 100, 100, 'd');
+	blecktangle.color = 'blue';
+	blecktangle.rotationSpeed = 5;
+	blecktangle.vel.x = -5;
+	blecktangle.friction = 2;
+	blecktangle.drag = 10;
+	blecktangle.bounciness = 100;
+	wallLH = new Sprite(0, height / 2, 8, height, 'k');
+	wallLH.color = 'black';
 
+	wallRH = new Sprite(width, height / 2, 8, height, 'k');
+
+	wallTop = new Sprite(width / 2, 0, width, 8, 'k');
+
+	wallBot = new Sprite(width / 2, height, width, 8, 'k');
+	ball_dot5 = new Sprite(width / 2, height / 2, 25, 'd');
+	ball_1 = new Sprite(width / 2, height / 2, 50, 'd');
+	ball_1dot5 = new Sprite(width / 2, height / 2, 75, 'd');
+	ball_2 = new Sprite(width / 2, height / 2, 100, 'd');
+	ball_3 = new Sprite(width / 2, height / 2, 150, 'd');
+	ball_4 = new Sprite(width / 2, height / 2, 200, 'd');
+	ball_5 = new Sprite(width / 2, height / 2, 250, 'd');
+	ball_6 = new Sprite(width / 2, height / 2, 300, 'd');
+
+	for (i = 10; i < 10; i++) {
+
+		alien = new Sprite(500, 300, 50, 50);
+
+		alien.vel.x = 3;
+
+		alien.vel.y = 4;
+
+		alien.bounciness = 1;
+
+		alien.friction = 0;
+
+	}
 }
-	
+
+
 /*******************************************************/
 // draw()
 /*******************************************************/
 function draw() {
-	
+	background("black")
 }
 
 /*******************************************************/
