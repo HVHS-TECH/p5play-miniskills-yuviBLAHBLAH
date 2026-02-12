fa07eec8f29db926fdf7909b7ee815b142499ab6
Your Name <23096yb@hvhs.school.nz>
2026-02-13 09:58:46 +1300

    aliens dont delete. fix this next spell

--- t50_extension.js
+++ t50_extension.js
@@ -3,20 +3,43 @@
 // Extension tasks
 // Written by ??
 /*******************************************************/
-	
+
 /*******************************************************/
 // setup()
 /*******************************************************/
 function setup() {
 	console.log("setup: ");
+width = random(300,600);
+height = random(300,600);
 
+// and the sprites keep changing too!
+spriteX= random(50,150);
+spriteY=  random(50,150);  ;
+
+cnv = new Canvas(width, height);
+
+
+red = new Sprite(spriteX/2, spriteY/2, spriteX, spriteY);
+red.color = 'red';
+
+green = new Sprite( width-50, spriteY/2 , spriteX, spriteY);
+green.color = 'green';
+
+blue = new Sprite( width-50, height-50, spriteX, spriteY);
+blue.color = 'blue';
+
+yellow = new Sprite( spriteX/2, height-50,  spriteX, spriteY);
+yellow.color = 'yellow';
+
+purple = new Sprite( width/2, height/2 ,  spriteX, spriteY);
+purple.color = 'purple';
 }
-	
+
 /*******************************************************/
 // draw()
 /*******************************************************/
 function draw() {
-	
+	background("cyan");
 }
 
 /*******************************************************/
